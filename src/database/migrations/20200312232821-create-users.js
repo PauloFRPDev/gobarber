7dae83255b12ea6c8b9cf8a40799de0c65aa658a
PauloFelippe <paulofelipperp.dev@gmail.com>
2020-03-12 20:43:02 -0300

    Created User's model

--- src/database/migrations/20200312232821-create-users.js
+++ src/database/migrations/20200312232821-create-users.js
@@ -11,6 +11,28 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 

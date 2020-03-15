4470238942a8ecc9313d73b16aa8f75e9f15338b
PauloFelippe <paulofelipperp.dev@gmail.com>
2020-03-15 17:40:09 -0300

    Feature: Added a new Queue of background jobs to send the cancellation email

---
+++ src/lib/Queue.js
@@ -0,0 +1,47 @@
+import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMail];
+
+class Queue {
+  constructor() {
+    this.queues = {};
+
+    this.init();
+  }
+
+  /**
+   * Initialize the queue of background jobs
+   */
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        handle,
+      };
+    });
+  }
+
+  /**
+   * Put a new job inside the background job queue
+   */
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  /**
+   * Process the jobs in background
+   */
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.process(handle);
+    });
+  }
+}
+
+export default new Queue();

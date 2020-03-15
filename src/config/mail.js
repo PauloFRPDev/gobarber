b47991ef4408df15c6fbb3f8c978870a98815f0f
PauloFelippe <paulofelipperp.dev@gmail.com>
2020-03-15 17:02:19 -0300

    Feature: Sending email telling the provider when an appointment is canceled

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'ddd18d34f5cf4e',
+    pass: 'c66625088bc0e7',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
